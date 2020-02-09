5e0d4c92b793a931477cd930721f64a862268b3b
Israel <israelb213@gmail.com>
2020-02-08 23:15:48 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgerDB;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Patty Melt");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
